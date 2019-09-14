80668575a18b5e475bcb9439812776a3a45b3126
Jhonnatan <jhonnatan133@gmail.com>
2019-09-13 23:23:49 -0500

    Updates

---
+++ precache-manifest.ea040b6029ee7e641cb4b34defcfcdc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68442d16f22b50fc33b0ea7946a43297",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "ac6976d605c3475f7e6b",
+    "url": "/calculator/static/css/main.c1b28034.chunk.css"
+  },
+  {
+    "revision": "86fc507f79de2a9d193f",
+    "url": "/calculator/static/js/2.0219f582.chunk.js"
+  },
+  {
+    "revision": "ac6976d605c3475f7e6b",
+    "url": "/calculator/static/js/main.32494ed5.chunk.js"
+  },
+  {
+    "revision": "335217aced5d2de02c3f",
+    "url": "/calculator/static/js/runtime~main.7273a9b5.js"
+  }
+]);
